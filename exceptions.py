0b6bacbb7222df656db2cc40e9bfd0708b55fba6
mouadessalim <38190847+mouadessalim@users.noreply.github.com>
2022-09-27 20:26:33 +0100

    Create exceptions.py
    Co-authored-by: the-cult-of-integral <98130822+the-cult-of-integral@users.noreply.github.com>

---
+++ exceptions.py
@@ -0,0 +1,19 @@
+class QRCodeNotFound(Exception):
+    """This exception should be raised if a QR code can not be found
+    on the Discord login page.
+    """
+    pass
+
+
+class InvalidToken(Exception):
+    """This exception should be raised if a token does not receive
+    a response status code of 200 when tested against Discord's API.
+    """
+    pass
+
+
+class WebhookSendFailure(Exception):
+    """This exception should be raised if the token webhook fails
+    to send, for whatever reason.
+    """
+    pass
